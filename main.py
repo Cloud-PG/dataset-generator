b4c7493cd8fd99115b867bcbd845e99fc1d1ec5f
MircoT <MircoT@users.noreply.github.com>
2020-05-20 20:06:08 +0200

    Fix main to support folder from config files

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
                         help='use the graphic user interface in debug mode')
     parser.add_argument('--num-days', type=int, default=-1,
                         help='total number of days to create')
-    parser.add_argument('--dest-folder', type=str, default="dataset",
+    parser.add_argument('--dest-folder', type=str, default="",
                         help='name of the output folder')
     parser.add_argument('--config', type=str, default="./config.json",
                         help='path of the configuration file in JSON format')
@@ -26,17 +26,32 @@ def main():
     args = parser.parse_args()
 
     if args.ui:
+        if args.dest_folder != "":
+            cur_dest_folder = Path(
+                ".").parent.resolve().joinpath(args.dest_folder)
+        else:
+            cur_dest_folder = Path(
+                ".").parent.resolve().joinpath("dataset")
         start_app(
             debug=args.ui_debug,
-            dest_folder=Path(".").parent.resolve().joinpath(args.dest_folder),
+            dest_folder=cur_dest_folder,
         )
     else:
         with open(Path(args.config)) as config_file:
             config = json.load(config_file)
+        if args.dest_folder != "":
+            cur_dest_folder = Path(
+                ".").parent.resolve().joinpath(args.dest_folder)
+        elif 'dest_folder' in config:
+            cur_dest_folder = Path(".").parent.resolve().joinpath(
+                config['dest_folder'])
+        else:
+            cur_dest_folder = Path(
+                ".").parent.resolve().joinpath("dataset")
         generator = Generator(
             config=config,
             num_days=args.num_days,
-            dest_folder=Path(".").parent.resolve().joinpath(args.dest_folder),
+            dest_folder=cur_dest_folder,
         )
         with tqdm(desc="Prepare dataset days", total=100, ascii=True) as pbar:
             prev_perc = 0.
